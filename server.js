a6e39f1b8966da27c49d2c43eb8f5e37957180e2
Azat Mardan <hi@azat.co>
2016-02-08 16:24:58 -0800

    add changes :de:

--- server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-var requestHandler = function(request, response) {
-  response.end('Hello World\n')
-}
-var server = http.createServer(requestHandler)
-server.listen(3000)
