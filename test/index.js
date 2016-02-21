14112071f3d38453a744201b7f553707f0dfcdee
Azat Mardan <hi@azat.co>
2016-02-21 11:48:35 -0800

    add changes :fr:

--- test/index.js
+++ test/index.js
@@ -11,7 +11,8 @@ before(function (done) {
 
 describe('server.js', function () {
   it('must use requestHandler', function(done){
-    var code = require('fs').readFileSync(require('path').join(__dirname, '..', 'server.js')).toString('utf-8')
+    var serverFile = require('path').join(__dirname, '..', 'server.js')
+    var code = require('fs').readFileSync(serverFile).toString('utf-8')
     expect(code).to.contain('requestHandler')
     done()
 
